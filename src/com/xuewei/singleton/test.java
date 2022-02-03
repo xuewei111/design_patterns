87141a0e672a379ad8ba789c414e6da9ece30c19
xuewei111 <m13358109269_1@163.com>
2022-02-03 20:35:42 +0800

    单例模式

---
+++ src/com/xuewei/singleton/test.java
@@ -0,0 +1,8 @@
+package com.xuewei.singleton;
+
+public class test {
+
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+    }
+}
---
+++ src/com/xuewei/singleton/type6/SingletonTest06.java
@@ -0,0 +1,33 @@
+package com.xuewei.singleton.type6;
+
+public class SingletonTest06 {
+
+    public static void main(String[] args) {
+        System.out.println("双重检查");
+        Singleton instance = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance == instance2); // true
+        System.out.println("instance.hashCode=" + instance.hashCode());
+        System.out.println("instance2.hashCode=" + instance2.hashCode());
+    }
+}
+// 懒汉式(线程安全，同步方法)
+class Singleton {
+    private static volatile Singleton singleton;
+
+    private Singleton (){};
+
+    //提供一个静态的公有方法，加入双重检查代码，解决线程安全问题, 同时解决懒加载问题
+    //同时保证了效率, 推荐使用
+    public static Singleton getInstance(){
+        if(singleton == null){
+            synchronized (Singleton.class) {
+                if(singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+
+        return singleton;
+    }
+}
