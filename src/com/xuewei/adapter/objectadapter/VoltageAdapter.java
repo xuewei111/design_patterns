54303d9e9023ae2caed47f811c34d5e2377dab13
xuewei111 <m13358109269_1@163.com>
2022-02-13 21:34:32 +0800

    修改

--- src/com/xuewei/adapter/objectadapter/VoltageAdapter.java
+++ src/com/xuewei/adapter/objectadapter/VoltageAdapter.java
@@ -12,7 +12,8 @@ public class VoltageAdapter extends Voltage220v implements IVoltage5V {
     public int output5V() {
         int dst = 0;
         if(null != voltage220v) {
-            int src = voltage220v.output220V();//获取220V 电压
+            //获取220V 电压
+            int src = voltage220v.output220V();
             System.out.println("使用对象适配器，进行适配~~");
             dst = src / 44;
             System.out.println("适配完成，输出的电压为=" + dst);
