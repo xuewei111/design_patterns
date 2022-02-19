945336da408e85645c3f090b8a93a7ddd6968ef8
xuewei111 <m13358109269_1@163.com>
2022-02-19 23:47:16 +0800

    外观模式

---
+++ src/com/xuewei/facade/Client.java
@@ -0,0 +1,14 @@
+package com.xuewei.facade;
+
+public class Client {
+
+    public static void main(String[] args) {
+        //这里直接调用。。 很麻烦
+        HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade();
+        homeTheaterFacade.ready();
+        homeTheaterFacade.play();
+
+
+        homeTheaterFacade.end();
+    }
+}
