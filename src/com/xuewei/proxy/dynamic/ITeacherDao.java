58db80ec76541e72387574de9e414ad3cb4c425c
xuewei111 <m13358109269_1@163.com>
2022-02-23 12:49:43 +0800

    代理模式(动态代理)

---
+++ src/com/xuewei/proxy/dynamic/ITeacherDao.java
@@ -0,0 +1,12 @@
+package com.xuewei.proxy.dynamic;
+
+//接口
+public interface ITeacherDao {
+
+    /**
+     * 授课方法
+     */
+    void teach();
+
+    void sayHello(String name);
+}
---
+++ src/com/xuewei/proxy/dynamic/TeacherDao.java
@@ -0,0 +1,13 @@
+package com.xuewei.proxy.dynamic;
+
+public class TeacherDao implements ITeacherDao{
+    @Override
+    public void teach() {
+        System.out.println(" 老师授课中.... ");
+    }
+
+    @Override
+    public void sayHello(String name) {
+        System.out.println("hello " + name);
+    }
+}
